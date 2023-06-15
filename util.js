79bdfc43c44a550370a8125cc11c6a41b2d4e8de
jercilla <joseba.ercilla@gmail.com>
2023-06-15 08:10:13 +0200

    Prompt segundo numero

--- util.js
+++ util.js
@@ -6,6 +6,6 @@ function obtenerSegundoNumero() {
   return 2
 }
 
-function obtenerSegundoNumeroTardandoMucho(seg = 5) {
-  return new Promise((r) => setTimeout(() => r(2), seg * 1000));
+function obtenerSegundoNumeroTardandoMucho(seg = 1) {
+  return new Promise((r) => setTimeout(() => r(prompt("Segundo número:")), seg * 1000));
 }
